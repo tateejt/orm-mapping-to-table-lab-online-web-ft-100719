f0b722fcb58eff4b9ab0fe573bf1c1e345463006
Emery Tate <tateejt@gmail.com>
2019-11-14 03:10:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   def self.create_table
     
     sql =  <<- SQL 
-          CREATE TABLE IF NOT EXITS students (
+        CREATE TABLE IF NOT EXITS students (
           id INTEGER PRIMARY KEY,
           name TEXT,
           grade TEXT
